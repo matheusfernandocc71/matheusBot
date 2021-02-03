c45adccba3259487ad072b61cbfebfef427cacb0
matheusfernandocc71 <77748393+matheusfernandocc71@users.noreply.github.com>
2021-02-03 05:12:57 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,161 @@
+
+const help = (prefix) => { 
+	return `
+┌─────────❶.❷────────┐
+   ™ BOT ZK SHITS OFF ™
+└─────────❶.❷────────┘
+ / / / / / / / / / / / / /
+╔══════════════════════
+║╭─────────────────────
+║├─────𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒──────
+║├─────────────────────
+║├> ${prefix}fig
+║├────> fazer fig cm foto/gif
+║├─────────────────────
+║├> ${prefix}tts seu txt
+║├────> converter txt em áudio
+║├─────────────────────
+║├> ${prefix}converter marca fig
+║├────> converter fig em foto
+║├─────────────────────
+║├> ${prefix}txtdafoto
+║├────> pega txt de foto
+║├─────────────────────
+║├> ${prefix}pesquisar marca foto
+║├────> pesquisa por anime de ft
+║├─────────────────────
+║├> ${prefix}adms
+║├────> lista dos adms
+║├─────────────────────
+║├> ${prefix}cachorro
+║├────> dog fofo aleatório
+║├─────────────────────
+║├> ${prefix}pokemon
+║├────> pokemon aleatório
+║├─────────────────────
+║├> ${prefix}logo3d seu txt
+║├────> logo cm seu texto
+║├─────────────────────
+║├> ${prefix}logoph seu|txt
+║├────> logo cm seu texto
+║├─────────────────────
+║├> ${prefix}info
+║├────> informações do bot
+║├─────────────────────
+║├> ${prefix}listabloq
+║├────> números bloqueados
+║├─────────────────────
+║├> ${prefix}lofi
+║├────> foto única de lofi
+║├─────────────────────
+║├> ${prefix}map 0000-0000
+║├────> pega foto do local
+║├─────────────────────
+║├> ${prefix}views
+║├────> aumente suas views
+║├─────────────────────
+║├──𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐎 𝐂𝐑𝐈𝐀𝐃𝐎𝐑──
+║├─────────────────────
+║├> ${prefix}hidetag
+║├────> esconde tag
+║├─────────────────────
+║├> ${prefix}limpar
+║├────> limpa zap do bot
+║├─────────────────────
+║├> ${prefix}bloquear @membro
+║├────> autobloq
+║├─────────────────────
+║├> ${prefix}desbloquear
+║├────> autodesbloq
+║├─────────────────────
+║├> ${prefix}kitar
+║├────> bot sai do grupo
+║├─────────────────────
+║├> ${prefix}bc
+║├────> [?]
+║├─────────────────────
+║├> ${prefix}clonar
+║├────> clonar tag
+║├─────────────────────
+║├──✔︎𝐄𝐗𝐂𝐋𝐔𝐒𝐈𝐕𝐎 𝐏𝐀𝐑𝐀 𝐀𝐃𝐌'𝐒✔︎─
+║├───────────────────── 
+║├> ${prefix}welcome [0/1]   
+║├────> desativar/ativa bv   
+║├─────────────────────      
+║├> ${prefix}add 55xxx
+║├────> add membro           
+║├─────────────────────
+║├> ${prefix}ban @membro
+║├────> remover membro
+║├─────────────────────
+║├> ${prefix}promover @membro
+║├────> promover a ADM
+║├─────────────────────
+║├> ${prefix}promover @adm
+║├────> rebaixar a membro
+║├─────────────────────
+║├> ${prefix}grupo abrir/fechar
+║├────> bem específico ;-;
+║├─────────────────────
+║├> ${prefix}mudarnome seu txt
+║├────> troca nome do grupo
+║├─────────────────────
+║├> ${prefix}mudardesc seu txt
+║├────> troca descrição
+║├─────────────────────
+║├> ${prefix}mudarfoto
+║├────> mudar foto do grupo
+║├─────────────────────
+║├> ${prefix}marcar
+║├────> marcação
+║├─────────────────────
+║├> ${prefix}marcar2
+║├────> marcação 2
+║├─────────────────────
+║├> ${prefix}marcar3
+║├────> marcação
+║├─────────────────────
+║├─✔︎𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃/𝐋𝐈𝐍𝐊✔︎─
+║├─────────────────────
+║├> ${prefix}tiktokstalk Nick da conta
+║├────> stalk de tiktok
+║├─────────────────────
+║├> ${prefix}fototiktok
+║├────> foto do tiktok
+║├─────────────────────
+║├> ${prefix}ytmp4 link
+║├────> baixa vídeo do YouTube
+║├─────────────────────
+║├─────✔︎𝐒𝐔𝐏𝐎𝐑𝐓𝐄✔︎─────
+║├─────────────────────
+║├> ${prefix}bug
+║├────> reportar bug ao criador
+║├─────────────────────
+║├> ${prefix}sigla nova sigla
+║├────> troca sigla de comandos
+║└─────────────────── 
+╚══════════════════════
+
+┌─────────❶.❷────────┐
+𝐂𝐑𝐈𝐀𝐃𝐎𝐑:𝙈𝘼𝙏𝙃𝙀𝙐𝙎𝙕𝙆
+𝙉𝙪́𝙢𝙚𝙧𝙤 𝙙𝙤 𝙘𝙧𝙞𝙖𝙙𝙤𝙧
+Wa.me//+5543991112879
+└─────────❶.❷────────┘
+
+Grupo: https://chat.whatsapp.com/CVpjMNftNqJEPi1D6tdl7A
+
+<══════════════════════>
+✔ 𝘽𝙊𝙏 𝙕𝙆 𝙎𝙃𝙄𝙏𝙎 𝐎𝐅𝐅 ︎✔︎ 
+<══════════════════════>
+
+█████████
+█▄█████▄█
+█▼▼▼▼▼
+█ □□□□□0%  ■□□□□20%  ■■□□□40%   ■■■■□80%   ■■■■■100%
+█▲▲▲▲▲
+█████████
+ ██ ██
+
+`
+}
+exports.help = help
