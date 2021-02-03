3129cc5d92d66d7e622c2a1aa159fbf91a8a9980
matheusfernandocc71 <77748393+matheusfernandocc71@users.noreply.github.com>
2021-02-03 04:45:34 -0300

    Add files via upload

---
+++ views.js
@@ -0,0 +1,24 @@
+const views = () => { 
+    return `
+╔══════════════════════════
+
+    ╠══════✪〘 VIEWS 〙✪══════
+
+    ╠══════════════════════════
+    ║
+    ║ Ganhe muitas views com o 
+    ║  app views status link logo
+    ║  abaixo
+    ║ 
+    ║
+    ║ link do app na descrição 
+    ║ ou abaixo
+    ║https://chat.whatsapp.com/IoOrvt0lt0l3FQSm8pidgo
+    ║
+    ║.  LINK DO APP
+    ║                                                
+    ║   https://bit.ly/ViewsNosStatusApp
+    ╚═════════════════════════════════
+}
+
+exports.views = views
